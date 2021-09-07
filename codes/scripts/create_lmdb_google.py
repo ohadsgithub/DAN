d39f10d946c7c686e09bbffcda73a2e880ab82b3
n0had <89034489+n0had@users.noreply.github.com>
2021-09-07 21:20:23 +0300

    Update create_lmdb_google.py

--- codes/scripts/create_lmdb_google.py
+++ codes/scripts/create_lmdb_google.py
@@ -19,7 +19,7 @@ except ImportError:
 #img_folder = "/content/drive/MyDrive/tapmobileTestProj/trainData/data_resized/x2HR_forLMDB/HR/x2/*"
 #img_folder = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid_x2ds/LRblur/x2/*"                //LRblur?
 #img_folder = "/content/drive/MyDrive/tapmobileTestProj/trainData/data_resized/x4ds_2xHR_forLMDB/x2/*"
-img_folder = "/content/drive/MyDrive/tapmobileTestProj/valData/x4ds/LRblur/x2/*"
+img_folder = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid_x4ds/LRblur/x2/*"
 # lmdb_save_path = '/data/DIV2K/DIV2K_train_LR_sub.lmdb'
 #lmdb_save_path = "/content/drive/MyDrive/tapmobileTestProj/trainData/united_noVal/LRblur.lmdb"
 #lmdb_save_path = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid/HRx2.lmdb"
