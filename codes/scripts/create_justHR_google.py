428c402f69274a8951b35d59733b9c51b77ef627
n0had <89034489+n0had@users.noreply.github.com>
2021-09-07 19:48:35 +0300

    Update create_justHR_google.py

--- codes/scripts/create_justHR_google.py
+++ codes/scripts/create_justHR_google.py
@@ -24,7 +24,7 @@ def create_justHR_google():
     #sourcedir = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid/LR/x2"
     #savedir = "/content/drive/MyDrive/tapmobileTestProj/valData"
     
-    sourcedir = "/content/drive/MyDrive/tapmobileTestProj/trainData/data_resized/x2ds/source"
+    sourcedir = "/content/drive/MyDrive/tapmobileTestProj/trainData/data_resized/x4ds/source"
     savedir = "/content/drive/MyDrive/tapmobileTestProj/trainData/data_resized"
 
     saveHRpath = os.path.join(savedir, "HR", "x" + str(mod_scale))
