6f09961a12eac37249ff15d2156d7a7c7c96703f
n0had <89034489+n0had@users.noreply.github.com>
2021-09-07 23:23:40 +0300

    Update blind_model.py

--- codes/config/DANv2/models/blind_model.py
+++ codes/config/DANv2/models/blind_model.py
@@ -266,11 +266,14 @@ class B_Model(BaseModel):
                   "DANv2_with_mobilenetV2.onnx",   # where to save the model (can be a file or file-like object)
                   export_params=True,              # store the trained parameter weights inside the model file
                   opset_version=10,                # the ONNX version to export the model to
-                  do_constant_folding=True,        # whether to execute constant folding for optimization
+                  #do_constant_folding=True,        # whether to execute constant folding for optimization
                   input_names = ['input'],         # the model's input names
                   output_names = ['output'],       # the model's output names
-                  dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
-                                'output' : {0 : 'batch_size'}})
+                  #dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
+                  #              'output' : {0 : 'batch_size'}}
+                         )
+        
+        
         
     #    x = torch.randn(batch_size, 1, 224, 224, requires_grad=True)
     #    torch_out = torch_model(x)
