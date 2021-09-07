10456d24d9f5b41bc7e5d3f0307144e26b320886
n0had <89034489+n0had@users.noreply.github.com>
2021-09-07 15:34:27 +0300

    Update generate_mod_blur_LR_bic_google.py

--- codes/scripts/generate_mod_blur_LR_bic_google.py
+++ codes/scripts/generate_mod_blur_LR_bic_google.py
@@ -19,8 +19,11 @@ def generate_mod_LR_bic():
     #sourcedir = "/content/drive/MyDrive/tapmobileTestProj/trainData/united_noVal/source"
     #savedir = "/content/drive/MyDrive/tapmobileTestProj/trainData/united_noVal"
     
-    sourcedir = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid/source"
-    savedir = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid"
+    #sourcedir = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid/source"
+    #savedir = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid"
+    
+    sourcedir = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid_x2ds/source"
+    savedir = "/content/drive/MyDrive/tapmobileTestProj/valData/DIV2K_valid_x2ds"
     
     # load PCA matrix of enough kernel
     print("load PCA matrix")
