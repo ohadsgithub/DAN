0321be03f707b191bc0a308ab3ef0f23b380520f
n0had <89034489+n0had@users.noreply.github.com>
2021-09-11 17:26:57 +0300

    Update tnn_altered.cc

--- cpp_TNN/tnn_altered.cc
+++ cpp_TNN/tnn_altered.cc
@@ -14,11 +14,12 @@ TNN::~TNN() {
 
 Status TNN::Init(ModelConfig& config) {
     impl_ = TNNImplManager::GetTNNImpl(config.model_type);
-    if (!impl_) {
+    //if (!impl_) {
+    if (True) {
         LOGE("Error: not support mode type: %d. If TNN is a static library, link it with option-Wl,--whole-archive tnn -Wl,--no-whole-archive on android or add -force_load on iOS\n", config.model_type);
         return Status(TNNERR_NET_ERR, "unsupported mode type, If TNN is a static library, link it with option -Wl,--whole-archive tnn -Wl,--no-whole-archive on android or add -force_load on iOS");
     }
-  fprintf("model type is %d", char(config.model_type));
+  //fprintf("model type is %d", char(config.model_type));
     return impl_->Init(config);
 }
 
