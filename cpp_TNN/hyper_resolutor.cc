a9e0b50e0b0233cc0bfa0b32d02f2d1e9638d10e
n0had <89034489+n0had@users.noreply.github.com>
2021-09-12 09:51:55 +0300

    Update hyper_resolutor.cc

--- cpp_TNN/hyper_resolutor.cc
+++ cpp_TNN/hyper_resolutor.cc
@@ -27,16 +27,13 @@ std::shared_ptr<Mat> HyperResolutor::ProcessSDKInputMat(std::shared_ptr<Mat> inp
         }
     }
   
-  
-  //return TNNSDKSample::ResizeToInputShape(input_mat, name); /// ??
-  
     return input_mat;
 }
 
 
 MatConvertParam HyperResolutor::GetConvertParamForInput(std::string tag) {
     MatConvertParam input_cvt_param;
-    input_cvt_param.scale = {1.0 / (255 * 0.229), 1.0 / (255 * 0.224), 1.0 / (255 * 0.225), 0.0}; ///////  ??
+    input_cvt_param.scale = {1.0 / (255 * 0.229), 1.0 / (255 * 0.224), 1.0 / (255 * 0.225), 0.0}; 
     input_cvt_param.bias  = {-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.225, 0.0};
     return input_cvt_param;
 }
@@ -48,30 +45,19 @@ std::shared_ptr<TNNSDKOutput> HyperResolutor::CreateSDKOutput() {
 Status HyperResolutor::ProcessSDKOutput(std::shared_ptr<TNNSDKOutput> output_) {
     Status status = TNN_OK;
   
-    //auto option = dynamic_cast<HyperResolutorOption *>(option_.get());
-    //RETURN_VALUE_ON_NEQ(!option, false,
-    //                    Status(TNNERR_PARAM_ERR, "TNNSDKOption is invalid"));
   
     auto output = dynamic_cast<HyperResolutorOutput *>(output_.get());
     RETURN_VALUE_ON_NEQ(!output, false,
                         Status(TNNERR_PARAM_ERR, "TNNSDKOutput is invalid"));
   
     
-    auto output_mat_patch = output->GetMat(); //////////////////////////////////////////////////////////////////////////////// here?
+    auto output_mat_patch = output->GetMat();
     RETURN_VALUE_ON_NEQ(!output_mat_patch, false,
                         Status(TNNERR_PARAM_ERR, "output_mat_scores is invalid"));
     
     
-    uint8_t *output_data_patch = (uint8_t*)output_mat_patch.get()->GetData(); /////////////////////////////// define in header file?
+    uint8_t *output_data_patch = (uint8_t*)output_mat_patch.get()->GetData(); 
   
-    //float max_v        = scores_data[0];
-    //for (int i = 1; i < output_mat_scores->GetChannel(); ++i) {
-    //    if (max_v < scores_data[i]) {
-    //        max_v    = scores_data[i];
-    //        class_id = i;
-    //    }
-    //}
-    //output->class_id = class_id;
   
     output->output_data_patch = output_data_patch;
   
