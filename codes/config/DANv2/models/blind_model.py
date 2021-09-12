ead429552d22484ef280a91762be410aaf66f8c8
n0had <89034489+n0had@users.noreply.github.com>
2021-09-12 09:34:39 +0300

    Update blind_model.py

--- codes/config/DANv2/models/blind_model.py
+++ codes/config/DANv2/models/blind_model.py
@@ -253,16 +253,13 @@ class B_Model(BaseModel):
         
         
     def makeONNX(self):
-        #copy1=nn.DataParallel(self.netG)
-        self.netG.eval() # maybe its already at eval?
+        
+        self.netG.eval()
         
         #randomInput = torch.randn( 1, 3, 255, 255, requires_grad=True)
         #randomInput = torch.randn( 1, 3, 255, 255)
-        randomInput = torch.randn( 1, 3, 255, 255).to(self.device)
-        
-        #torch_out = self.netG(randomInput) # is this even needed?
+        randomInput = torch.randn( 1, 3, 255, 255).to(self.device) 
 
-        # Export the model
         #self.netG.module or just self.netG ?
         torch.onnx.export(self.netG.module,        # model being run
                   randomInput,                     # model input (or a tuple for multiple inputs)
@@ -275,21 +272,5 @@ class B_Model(BaseModel):
                   #dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
                   #              'output' : {0 : 'batch_size'}}
                          )
-        
-        
-        
-    #    x = torch.randn(batch_size, 1, 224, 224, requires_grad=True)
-    #    torch_out = torch_model(x)
-
-        # Export the model
-    #    torch.onnx.export(torch_model,               # model being run
-    #              x,                         # model input (or a tuple for multiple inputs)
-    #              "super_resolution.onnx",   # where to save the model (can be a file or file-like object)
-    #              export_params=True,        # store the trained parameter weights inside the model file
-    #              opset_version=10,          # the ONNX version to export the model to
-    #              do_constant_folding=True,  # whether to execute constant folding for optimization
-    #              input_names = ['input'],   # the model's input names
-    #              output_names = ['output'], # the model's output names
-    #              dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
-    #                            'output' : {0 : 'batch_size'}})
+ 
         
