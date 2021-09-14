af0c24a79f5e900631dd69b3b03aa3ad705792da
ohadsgithub <90630277+ohadsgithub@users.noreply.github.com>
2021-09-14 16:28:08 +0300

    Update ONNX_from_saved_model.py

--- codes/config/DANv2/ONNX_from_saved_model.py
+++ codes/config/DANv2/ONNX_from_saved_model.py
@@ -20,16 +20,24 @@ import utils as util
 from data import create_dataloader, create_dataset
 from data.util import bgr2ycbcr
 
+
+import torch.onnx
+
+
 #### options
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "-opt",
     type=str,
-    default="options/setting2/test_setting2_x4.yml",
+    default="options/setting1/test/test_setting1_x2.yml",
     help="Path to options YMAL file.",
 )
-parser.add_argument("-input_dir", type=str, default="../../../data_samples/LR")
-parser.add_argument("-output_dir", type=str, default="../../../data_samples/DANv2")
+
+#default="options/setting2/test_setting2_x4.yml",
+
+#parser.add_argument("-input_dir", type=str, default="../../../data_samples/LR")
+#parser.add_argument("-output_dir", type=str, default="../../../data_samples/DANv2")
+
 args = parser.parse_args()
 opt = option.parse(args.opt, is_train=False)
 
@@ -37,22 +45,30 @@ opt = option.dict_to_nonedict(opt)
 
 model = create_model(opt)
 
-if not osp.exists(args.output_dir):
-    os.makedirs(args.output_dir)
+model.makeONNX()
 
-test_files = glob(osp.join(args.input_dir, "*png"))
-for inx, path in tqdm(enumerate(test_files)):
-    name = path.split("/")[-1].split(".")[0]
+#if not osp.exists(args.output_dir):
+#    os.makedirs(args.output_dir)
 
-    img = cv2.imread(path)[:, :, [2, 1, 0]]
-    img = img.transpose(2, 0, 1)[None] / 255
-    img_t = torch.as_tensor(np.ascontiguousarray(img)).float()
+#test_files = glob(osp.join(args.input_dir, "*png"))
+#for inx, path in tqdm(enumerate(test_files)):
+    #name = path.split("/")[-1].split(".")[0]
 
-    model.feed_data(img_t)
-    model.test()
+    #img = cv2.imread(path)[:, :, [2, 1, 0]]
+    
+    
+    
+    #####img = img.transpose(2, 0, 1)[None] / 255
+    
+    #####img_t = torch.as_tensor(np.ascontiguousarray(img)).float()
 
-    sr = model.fake_SR.detach().float().cpu()[0]
-    sr_im = util.tensor2img(sr)
+    
+    
+    #model.feed_data(img_t)
+    #model.test()
 
-    save_path = osp.join(args.output_dir, "{}_x{}.png".format(name, opt["scale"]))
-    cv2.imwrite(save_path, sr_im)
+    #sr = model.fake_SR.detach().float().cpu()[0]
+    #sr_im = util.tensor2img(sr)
+
+    #save_path = osp.join(args.output_dir, "{}_x{}.png".format(name, opt["scale"]))
+    #cv2.imwrite(save_path, sr_im)
