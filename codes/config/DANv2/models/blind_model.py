e964d011ba54e8ead5920eee7dfb6ac93b9dd40c
ohadsgithub <90630277+ohadsgithub@users.noreply.github.com>
2021-09-20 13:25:14 +0300

    Update blind_model.py

--- codes/config/DANv2/models/blind_model.py
+++ codes/config/DANv2/models/blind_model.py
@@ -277,11 +277,67 @@ class B_Model(BaseModel):
                   #dynamic_axes={'input' : {0 : 'batch_size'},    # variable length axes
                   #              'output' : {0 : 'batch_size'}}
                          )
-        
+ 
+
+
     def print_named_parameters(self):
         #self.netG.
         #module = model.conv1
         print(list(self.netG.named_parameters()))
+   
+
+    
+    def measure_module_sparsity(module, weight=True, bias=False, use_mask=False):
+
+        num_zeros = 0
+        num_elements = 0
+
+        if use_mask == True:
+            for buffer_name, buffer in module.named_buffers():
+                if "weight_mask" in buffer_name and weight == True:
+                    num_zeros += torch.sum(buffer == 0).item()
+                    num_elements += buffer.nelement()
+                if "bias_mask" in buffer_name and bias == True:
+                    num_zeros += torch.sum(buffer == 0).item()
+                    num_elements += buffer.nelement()
+        else:
+            for param_name, param in module.named_parameters():
+                if "weight" in param_name and weight == True:
+                    num_zeros += torch.sum(param == 0).item()
+                    num_elements += param.nelement()
+                if "bias" in param_name and bias == True:
+                    num_zeros += torch.sum(param == 0).item()
+                    num_elements += param.nelement()
+
+        sparsity = num_zeros / num_elements
+
+        return num_zeros, num_elements, sparsity
+    
+    
+    
+    def measure_global_sparsity(model,
+                            weight=True,
+                            bias=False,
+                            conv2d_use_mask=False,
+                            linear_use_mask=False):
+
+        num_zeros = 0
+        num_elements = 0
+
+        for module_name, module in model.named_modules():
+
+            if isinstance(module, torch.nn.Conv2d):
+
+                module_num_zeros, module_num_elements, _ = measure_module_sparsity(
+                    module, weight=weight, bias=bias, use_mask=conv2d_use_mask)
+                num_zeros += module_num_zeros
+                num_elements += module_num_elements
+
+        sparsity = num_zeros / num_elements
+
+        return num_zeros, num_elements, sparsity
+  
+
         
     def pruneConvolutions(someParameters):
         asdf=1
